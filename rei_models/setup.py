069d58ce972e06c42da688ca1207a7d059e28a6d
Joshua <joshkafeero@gmail.com>
2024-09-12 13:02:14 -0400

    Initial commits

--- rei_models/setup.py
+++ rei_models/setup.py
@@ -3,9 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="rei_models",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(include=['rei_models', 'rei_models.*']),
     install_requires=[
-        "pydantic[email] ~ = 2.3.",
+        "pydantic[email]~=2.3.0",
         "python-dateutil~=2.9.0.post0"
     ],
     author="Zimba Developers",
