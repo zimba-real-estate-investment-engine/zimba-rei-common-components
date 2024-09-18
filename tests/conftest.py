71a974767999d990c60b1628b2da830d59ead9f5
Joshua <joshkafeero@gmail.com>
2024-09-17 20:16:42 -0400

    Added investor profiles.

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ from dateutil.relativedelta import relativedelta
 
 from rei_models import Listing
 from rei_models import Deal
+from rei_models import InvestorProfile
 # from rei_models.rei_models.Underwriting import Underwriting
 # from rei_models.rei_models.Mortgage import Mortgage
 
@@ -25,23 +26,23 @@ def get_test_listing_object() -> Listing:
     return listing
 
 
-# @pytest.fixture
-# def get_test_investor_profile_object() -> InvestorProfile:
-#     current_time_string = __get_time_string()
-#     f_name = "fname" + current_time_string
-#     l_name = "lname" + current_time_string
-#
-#     investor_profile = InvestorProfile(
-#         id=current_time_string, price=300000, first_name=f_name, last_name=l_name,
-#         email="email@example.com", title="Ms.", phone="1-888-454-1234",
-#         preferred_property_type="rental", preferred_locations=["SE", "NE"],
-#         bedrooms_max=8, bedrooms_min=2, bathrooms_min=2, bathrooms_max=3, budget_max=100000000, budget_min=200000,
-#         years_built_max=80, years_built_min=30, investment_purpose="rental",
-#         assigned_parking_required=True, central_heat_required=True, dishwasher_required=True,
-#         balcony_required=True
-#     )
-#     return investor_profile
-#
+@pytest.fixture
+def get_test_investor_profile_object() -> InvestorProfile:
+    current_time_string = __get_time_string()
+    f_name = "fname" + current_time_string
+    l_name = "lname" + current_time_string
+
+    investor_profile = InvestorProfile(
+        id=current_time_string, price=300000, first_name=f_name, last_name=l_name,
+        email="email@example.com", title="Ms.", phone="1-888-454-1234",
+        preferred_property_type="rental", preferred_locations=["SE", "NE"],
+        bedrooms_max=8, bedrooms_min=2, bathrooms_min=2, bathrooms_max=3, budget_max=100000000, budget_min=200000,
+        years_built_max=80, years_built_min=30, investment_purpose="rental",
+        assigned_parking_required=True, central_heat_required=True, dishwasher_required=True,
+        balcony_required=True
+    )
+    return investor_profile
+
 
 @pytest.fixture
 def get_test_deal_object() -> Deal:
--- tests/test_conftest.py
+++ tests/test_conftest.py
@@ -8,11 +8,11 @@ def test_get_current_time_in_seconds_string(get_current_time_in_seconds_string):
 def test_get_test_listing_object(get_test_listing_object):
     assert get_test_listing_object.square_feet != 0;
     assert get_test_listing_object.listing_date is not None
-#
-#
-# def test_investor_profile_object(get_test_investor_profile_object):
-#     assert get_test_investor_profile_object.first_name;
-#     assert get_test_investor_profile_object.last_name;
+
+
+def test_investor_profile_object(get_test_investor_profile_object):
+    assert get_test_investor_profile_object.first_name;
+    assert get_test_investor_profile_object.last_name;
 
 
 def test_deal_object(get_test_deal_object):
