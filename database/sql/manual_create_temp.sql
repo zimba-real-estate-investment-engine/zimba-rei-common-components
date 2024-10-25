0ab5c7c31f45b31408ae7f8ff623998efbaca0b5
Joshua <joshkafeero@gmail.com>
2024-10-25 10:37:49 -0400

    Added the boolean field to quickly check if the subscription is active.

--- database/sql/manual_create_temp.sql
+++ database/sql/manual_create_temp.sql
@@ -1,9 +1,10 @@
-CREATE TABLE subscription (
+CREATE TABLE IF NOT EXISTS subscription (
     id VARCHAR(255) NOT NULL PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
     service_subscribed_to VARCHAR(255),
     source_url VARCHAR(255),
+    subscribed BOOLEAN,
     form_id VARCHAR(255),
     unsubscribed_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     unsubscribe_token VARCHAR(255),
