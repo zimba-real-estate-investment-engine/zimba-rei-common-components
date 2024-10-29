f2f75553e8400f27fe847ff52908c24401555976
Joshua <joshkafeero@gmail.com>
2024-10-29 14:36:52 -0400

    Interim commit

--- subscriptions_api/rei_models/__init__.py
+++ subscriptions_api/rei_models/__init__.py
@@ -3,3 +3,4 @@ from .InvestorProfile import InvestorProfile
 from .Deal import Deal
 from .Underwriting import Underwriting
 from .Subscription import Subscription
+from .Mortgage import Mortgage
--- underwritings_api/rei_models/__init__.py
+++ underwritings_api/rei_models/__init__.py
@@ -2,4 +2,5 @@ from .Listing import Listing
 from .InvestorProfile import InvestorProfile
 from .Deal import Deal
 from .Underwriting import Underwriting
+from .Subscription import Subscription
 from .Mortgage import Mortgage
