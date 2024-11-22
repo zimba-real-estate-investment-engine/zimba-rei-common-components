788192a033b36ef42e9ec5c78d0cca10b1e9236d
Joshua <joshkafeero@gmail.com>
2024-11-21 19:46:59 -0500

    Confirmed emails are being sent.

---
+++ app/schemas/EmailSchema.py
@@ -0,0 +1,11 @@
+from typing import List, Optional
+
+from pydantic import BaseModel, EmailStr
+
+
+class EmailSchema(BaseModel):
+    to_addresses: List[EmailStr]
+    subject: str
+    body_text: str
+    body_html: Optional[str] = None
+    sender: Optional[EmailStr] = None
--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ from app.models.SubscriptionModel import SubscriptionModel
 
 import pytest
 
+from app.schemas.EmailSchema import EmailSchema
 from app.schemas.AddressSchema import AddressSchema
 from app.schemas.ExpenseSchema import ExpenseSchema
 from app.schemas.RealEstatePropertySchema import RealEstatePropertySchema
@@ -162,6 +163,20 @@ def get_test_expense_schema() -> ExpenseSchema:
 
     return expense_schema
 
+
+@pytest.fixture
+def get_test_email_schema() -> EmailSchema:
+    current_time_string = __get_time_string()
+    expense_type = current_time_string + '_expense_type'
+
+    email_schema = EmailSchema(
+        to_addresses=['rei@zimbasolutions.io'], subject='SES Email Unit Test', sender='rei@zimbasolutions.io',
+        body_text='SES Unit Test Email body',
+    )
+
+    return email_schema
+
+
 @pytest.fixture
 def get_test_real_estate_property_schema() -> RealEstatePropertySchema:
     current_time_string = __get_time_string()
@@ -172,6 +187,7 @@ def get_test_real_estate_property_schema() -> RealEstatePropertySchema:
     )
 
     return expense_schema
+
 
 @pytest.fixture
 def get_test_subscription_schema() -> SubscriptionSchema:
