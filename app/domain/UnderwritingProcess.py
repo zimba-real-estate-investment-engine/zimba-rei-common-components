fbb9493b3a8040547f0e5ef2976517bf8c708379
Joshua <joshkafeero@gmail.com>
2024-12-06 15:12:36 -0500

    Draft logic for underwriting process

---
+++ app/domain/UnderwritingProcess.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from app.domain.Deal import Deal
+from app.domain.Expense import Expense
+from app.domain.InvestorProfile import InvestorProfile
+from app.domain.Listing import Listing
+from app.domain.RealEstateProperty import RealEstateProperty
+
+
+class UnderwritingProcess():
+
+    def create_deal(self, investor_profile: InvestorProfile, real_estate_property: RealEstateProperty) -> Deal:
+        pass
+
+    def extract_real_estate_property(self, listing: Listing, expenses: List[Expense]) -> RealEstateProperty:
+        pass
+
+    def extract_listing(self, uri: str) -> Listing:
+        pass
+
+    # def generate_underwriting(self, deal: Deal, investor: InvestorProfile,
+    #                           real_estate_property: RealEstateProperty) -> Underwriting:
+    #     pass
+    #
+    # def generate_project(self, uri: str) -> Projection:
+    #     pass
